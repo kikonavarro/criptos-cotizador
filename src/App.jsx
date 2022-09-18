a66e3f194b6be1ae008038fd2687680e7de557e7
kikonavarro <78880702+kikonavarro@users.noreply.github.com>
2022-09-18 17:54:43 +0200

    creando resultado y spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 import Formulario from './components/Formulario'
 
 
@@ -43,6 +45,32 @@ const Heading = styled.h1`
 function App() {
 
   const [ monedas, setMonedas ] = useState({})
+  const [ resultado, setResultado ] = useState({})
+  const [ cargando, setCargando ] = useState(false)
+
+
+  useEffect(() => {
+    if(Object.keys(monedas).length > 0) {
+        
+      const cotizarCripto = async () => {
+          setCargando(true)
+          setResultado({})
+
+          const { moneda, criptomoneda } = monedas
+          const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+
+          const respuesta = await fetch(url)
+          const resultado = await respuesta.json()
+
+          setResultado(resultado.DISPLAY[criptomoneda][moneda])
+
+          setCargando(false)
+      }
+
+      cotizarCripto()
+    }
+}, [monedas])
+
 
  
   return (
@@ -53,8 +81,12 @@ function App() {
             alt="imagenes criptomonedas"
           />
       <div>
-        <Heading>hola</Heading>
+        <Heading>Cotizador de Criptomonedas</Heading>
         <Formulario setMonedas={setMonedas}/>
+
+        {cargando && <Spinner />}
+        {resultado.PRICE && <Resultado resultado={resultado} />} 
+
       </div>
     </Contenedor>
   )
